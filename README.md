512059c820a39238f22d530355b8f3349a2e479d
Deepti Mahisare <deeptimahisare@gmail.com>
2024-11-28 16:16:12 +0530

    Readme update and insert.jsp added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Spring_MVC
+<h1> Spring_MVC</h1>
--- Spring_MVC_1/src/main/java/com/mypackage/Controller.java
+++ Spring_MVC_1/src/main/java/com/mypackage/Controller.java
@@ -1,7 +1,11 @@
 package com.mypackage;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -19,5 +23,23 @@ public class Controller {
 	String get2() {
 		
 		return "First";
+	}
+	
+	
+	@RequestMapping("/insert")
+
+	String insert(HttpServletRequest req ,Model m) {
+		
+		String roll = req.getParameter("roll");
+		String name = req.getParameter("name");
+		String per = req.getParameter("per");
+		
+		
+		
+		m.addAttribute("roll", roll);
+		m.addAttribute("name", name);
+		m.addAttribute("per", per);
+		
+		return "Insert";
 	}
 }
